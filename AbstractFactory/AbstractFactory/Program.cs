2972158e80466ee009d7cd5cfd140ea19f61bf4c
Tran Nhu Quynh <131272971+Binarosie@users.noreply.github.com>
2023-12-06 13:23:40 +0700

    Add files via upload

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IComputerFactory pcFactory = new PCFactory();
+
+            ComputerAssembler pcAssembler = new ComputerAssembler(pcFactory);
+            pcAssembler.AssembleComputer();
+
+            IComputerFactory workstationFactory = new WorkstationFactory();
+            ComputerAssembler workstationAssembler = new ComputerAssembler(workstationFactory);
+            workstationAssembler.AssembleComputer();
+        }
+    }
+}
